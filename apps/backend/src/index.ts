1f49d9bbdfa62cadbbb49f60b10a06541f516519
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-21 23:57:32 +0530

    express&ws_init

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,5 @@
-console.log("hi")
+import express from "express"
+
+const app = express();
+const PORT = 3000
+app.listen(PORT)
--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hi")
+import {WebSocketServer} from "ws";
+
+const wss = new WebSocketServer({port:8080});
+
+wss.on('connection', function connection(ws){
+    ws.on('message', function message(data){
+        ws.send('pong')
+    });
+});
+
