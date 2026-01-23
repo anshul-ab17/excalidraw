d7e062a36ed993671504c9bd14a21c5e37b0a667
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-23 23:36:31 +0530

    htpp-skeleton

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,42 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
+app.use(express.json());
+ 
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+ 
+app.post("/boards", (req, res) => {
+  res.sendStatus(201);
+});
+ 
+app.get("/boards/:boardId", (req, res) => {
+  res.sendStatus(200);
+});
+ 
+app.delete("/boards/:boardId", (req, res) => {
+  res.sendStatus(204);
+});
+ 
+app.get("/boards/:boardId/elements", (req, res) => {
+  res.sendStatus(200);
+});
+ 
+app.post("/boards/:boardId/elements", (req, res) => {
+  res.sendStatus(200);
+});
+ 
+app.delete("/boards/:boardId/elements/:elementId", (req, res) => {
+  res.sendStatus(204);
+});
 
+ 
 
+ 
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
