58d556d561c2d495e9e1307457c9958648688072
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-23 23:39:34 +0530

    ws-skeleton

--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -1,10 +1,20 @@
-import {WebSocketServer} from "ws";
+import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port:8080});
+const wss = new WebSocketServer({ port: 3000 });
+const rooms = new Map(); //-> Set<ws>
 
-wss.on('connection', function connection(ws){
-    ws.on('message', function message(data){
-        ws.send('pong')
-    });
+wss.on("connection", (ws) => {
+  let boardId = null;
+
+  ws.on("message", (data) => {
+    // join room
+    // leave room
+    // broadcast to room
+  });
+
+  ws.on("close", () => {
+    // cleanup room
+  });
 });
 
+console.log("WS server running on ws://localhost:3000");
