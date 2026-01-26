e0e5c6f02f0da22696f2d9d4f4cea0e02073bea3
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-26 23:48:21 +0530

    ws_jwt

--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -1,15 +1,26 @@
 import { WebSocketServer } from "ws";
+import { JWT_SECRET } from "./confi";
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 const wss = new WebSocketServer({ port: 3000 });
-const rooms = new Map(); //-> Set<ws>
 
-wss.on("connection", (ws) => {
-  let boardId = null;
+wss.on("connection", function connection(ws, request){
 
-  ws.on("message", (data) => {
-    // join room
-    // leave room
-    // broadcast to room
+  const url = request.url;
+  if(!url){
+    return;
+  }
+
+  const queryParam = new URLSearchParams(url.split('?')[1]);
+  const token = queryParam.get('token') || "";
+  const decode= jwt.verify(token,JWT_SECRET);
+
+  if(!decode || !(decode as JwtPayload).userId){
+    ws.close();
+    return;
+  }
+  ws.on("message", function message (data) {
+    ws.send('pong');
   });
 
   ws.on("close", () => {
@@ -17,4 +28,4 @@ wss.on("connection", (ws) => {
   });
 });
 
-console.log("WS server running on ws://localhost:3000");
+console.log("WS server running.");
